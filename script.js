9d4ebbffbce76bcb1f3ef695e9b6d7d3cb7b7618
= <=>
2022-10-09 01:52:49 +0530

    Add event listener for text selection over any part of document.body

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 document.body.style = "border : 10px solid red";
+
+document.body.addEventListener('mouseup', function(){
+   let selection = window.getSelection();
+    const final = selection.toString();
+    console.log(final)
+})
